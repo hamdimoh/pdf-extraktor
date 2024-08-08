385495b578acce989fd57281946022a54fda4986
mohammedal-hamdi <mohammed.abdullah.alhamdi@gmail.com>
2024-08-08 19:21:08 +0200

    Fix declarative_base() DB

--- 🏠Homepage.py
+++ 🏠Homepage.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, String, Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
